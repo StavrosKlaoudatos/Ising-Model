f090edb6e3947299a5800e935743a385a733818b
Stavros Klaoudatos <sklaouda@caltech.edu>
2024-03-31 02:10:08 +0200

    Updated exec

--- exec.sh
+++ exec.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+#Add your paths here
 python /Users/stavrosklaoudatos/Desktop/MyWork/Physics/Ising-Model/ising-model-webapp/app.py&
 open -a "Google Chrome" /Users/stavrosklaoudatos/Desktop/MyWork/Physics/Ising-Model/ising-model-webapp/index.html
