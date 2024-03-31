7a226f71365a5c9e162bfaf535e085c29a63109c
Stavros Klaoudatos <sklaouda@caltech.edu>
2024-03-31 02:08:58 +0200

    Updated with exec

---
+++ exec.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python /Users/stavrosklaoudatos/Desktop/MyWork/Physics/Ising-Model/ising-model-webapp/app.py&
+open -a "Google Chrome" /Users/stavrosklaoudatos/Desktop/MyWork/Physics/Ising-Model/ising-model-webapp/index.html
